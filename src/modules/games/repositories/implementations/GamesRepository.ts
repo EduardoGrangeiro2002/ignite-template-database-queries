b95e699ff8e4cf64dce681da96dce9a3f440dbb0
EduardoGrangeiro2002 <eduatdoaugusto503@gmail.com>
2022-01-04 21:24:47 -0300

    Arrumando o countAllGames

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -18,12 +18,11 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT (*) FROM games `); 
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games").where("games.id ").getMany
   }
 }
